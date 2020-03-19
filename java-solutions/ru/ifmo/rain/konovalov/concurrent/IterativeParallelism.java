ecedd0afc4e05188a3243aba72f4cad3d2e2de18
Geny200 <geny200k.other@mail.ru>
2020-03-19 19:51:14 +0300

    Concurrent update with correct import

--- java-solutions/ru/ifmo/rain/konovalov/concurrent/IterativeParallelism.java
+++ java-solutions/ru/ifmo/rain/konovalov/concurrent/IterativeParallelism.java
@@ -1,5 +1,7 @@
 package ru.ifmo.rain.konovalov.concurrent;
 
+import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;

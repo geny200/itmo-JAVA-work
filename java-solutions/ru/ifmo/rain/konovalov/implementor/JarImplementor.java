7f335e2e08050f654af8c04ab92dfbe5f5324d4e
Geny200 <geny200k.other@mail.ru>
2020-03-25 01:11:03 +0300

    Implementor (5-6 HW) new script fo javadoc

--- java-solutions/ru/ifmo/rain/konovalov/implementor/JarImplementor.java
+++ java-solutions/ru/ifmo/rain/konovalov/implementor/JarImplementor.java
@@ -17,7 +17,9 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.function.Function;
 import java.util.jar.Attributes;
 import java.util.jar.JarOutputStream;
@@ -26,7 +28,31 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.zip.ZipEntry;
 
+/**
+ * Implementation of {@link JarImpler} interface.
+ *
+ * @author Geny200
+ * @see JarImpler
+ * @see info.kgeorgiy.java.advanced.implementor.Impler
+ * @see ImplerException
+ */
 public class JarImplementor implements JarImpler {
+    /**
+     * Creates an implementation for interfaces depending on input parameters.
+     * Use to start:
+     * <ul>
+     *         <li> {@code java <class_name> -jar <class_to_implement> <jar_file>}
+     *         calls {@link JarImplementor#implementJar(Class, Path)}
+     *         </li>
+     *         <li> {@code java <class_name> <class_to_implement> <path_to_impl>}
+     *         calls {@link JarImplementor#implement(Class, Path)}
+     *         </li>
+     * </ul>
+     *
+     * @param args array of input parameters ({@link java.lang.String}).
+     * @see JarImplementor#implementJar(Class, Path)
+     * @see JarImplementor#implement(Class, Path)
+     */
     public static void main(String[] args) {
         if (args == null || args.length < 2 || args[0] == null || args[1] == null || args.length >= 3 && args[2] == null) {
             System.out.println("Error: null input argument");
@@ -52,6 +78,11 @@ public class JarImplementor implements JarImpler {
         }
     }
 
+    /**
+     * Returns class path.
+     *
+     * @param token {@link java.lang.Class} the class for which the path is required.
+     */
     private static String getClassPath(Class<?> token) {
         try {
             return Path.of(token.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
@@ -62,34 +93,74 @@ public class JarImplementor implements JarImpler {
         }
     }
 
+    /**
+     * Returns the full name of the implementation class.
+     *
+     * @param token {@link java.lang.Class} a class for which the full name of its implementation is required.
+     */
     private static String getImplName(final Class<?> token) {
         return token.getPackageName() + "." + token.getSimpleName() + "Impl";
     }
 
-    public static Path getFile(final Path root, final Class<?> clazz) {
-        return root.resolve(getImplName(clazz).replace(".", File.separator) + ".java").toAbsolutePath();
-    }
-
-    public static void compileFiles(final Path root, final String file, Class<?> token) {
+    /**
+     * Compiles a {@link java.lang.Class} from its implementation. The compiled file is placed along the path <var>root</var>..
+     *
+     * @param root {@link java.nio.file} for the root folder to compile.
+     */
+    public static void compile(final Path root, final Class<?> token) {
         final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        String[] argsStr = {"-encoding", "cp866", "-cp", root + File.pathSeparator + getClassPath(token), file};
+        String[] argsStr = {
+                "-encoding",
+                "cp866",
+                "-cp",
+                root + File.pathSeparator + getClassPath(token),
+                root.resolve(getImplName(token).replace(".", File.separator) + ".java").toAbsolutePath().toString()
+        };
         final int exitCode = compiler.run(null, null, null, argsStr);
     }
 
-    public static void compile(final Path root, final Class<?> token) {
-        compileFiles(root, getFile(root, token).toString(), token);
+    /**
+     * Changes to unicode encoding.
+     *
+     * @param str the {@link java.lang.String} of which you need to change the encoding to unicode
+     */
+    private String toUnicode(String str) {
+        StringBuilder escapeBuilder = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (c >= 128)
+                escapeBuilder.append(String.format("\\u%04X", (int) c));
+            else
+                escapeBuilder.append(c);
+        }
+        return escapeBuilder.toString();
     }
 
+    /**
+     * Creates a list of parameters that can be numbered by passing the corresponding parameter <var>toStr</var>.
+     *
+     * @param clazz A {@link java.lang.Class} - array of tokens - types.
+     * @param toStr A {@link java.util.function} - generates parameter name based on number.
+     */
     private String getStrFromArray(Class<?>[] clazz, Function<Integer, String> toStr) {
         return IntStream.range(0, clazz.length).mapToObj(i -> clazz[i].getCanonicalName() + toStr.apply(i + 1)).collect(Collectors.joining(", "));
     }
 
+    /**
+     * Returns the implementation of throws with the type to which the in {@link java.lang.Class[]} belongs.
+     *
+     * @param clazz A {@link java.lang.Class} - array of tokens - types
+     */
     private String getExceptions(Class<?>[] clazz) {
         if (clazz.length == 0)
             return "";
         return " throws " + getStrFromArray(clazz, i -> "");
     }
 
+    /**
+     * Returns the implementation of return with the type to which the {@link java.lang.Class} belongs.
+     *
+     * @param clazz A {@link java.lang.Class} token.
+     */
     private String getReturn(Class<?> clazz) {
         if (void.class.equals(clazz))
             return "return";
@@ -101,6 +172,14 @@ public class JarImplementor implements JarImpler {
             return "return null";
     }
 
+    /**
+     * Returns the implementation of {@link java.lang.reflect.Executable} in the form of a {@link java.lang.String}.
+     *
+     * @param method     A {@link java.lang.reflect.Executable} for which implementation is required.
+     * @param name       A {@link java.lang.String} - field name.
+     * @param returnName A {@link java.lang.String} - return type name.
+     * @param body       A {@link java.lang.String} - function body.
+     */
     private String getStrFromExecutable(Executable method, String name, String returnName, String body) {
         return String.format(
                 "\t%s %s %s(%s)%s{ \n\t\t%s;\n\t}\n\n",
@@ -113,6 +192,11 @@ public class JarImplementor implements JarImpler {
         );
     }
 
+    /**
+     * Returns the implementation of {@link java.lang.reflect.Method} in the form of a {@link java.lang.String}.
+     *
+     * @param method A {@link java.lang.reflect.Method} for which implementation is required.
+     */
     private String getStrFromMethod(Method method) {
         return getStrFromExecutable(
                 method,
@@ -122,6 +206,12 @@ public class JarImplementor implements JarImpler {
         );
     }
 
+    /**
+     * Returns the implementation of class constructor in the form of a {@link java.lang.String}.
+     *
+     * @param className   A {@link java.lang.String} class name.
+     * @param constructor A {@link java.lang.reflect.Constructor} class constructor.
+     */
     private String getConstructor(String className, Constructor<?> constructor) {
         if (constructor == null)
             return "";
@@ -135,12 +225,23 @@ public class JarImplementor implements JarImpler {
                                 .collect(Collectors.joining(", "))));
     }
 
+    /**
+     * Returns the package of the {@link java.lang.Class} if it exists.
+     *
+     * @param token A {@link java.lang.Class} token.
+     * @see Class#getPackage()
+     */
     private String getPackage(Class<?> token) {
         if (token.getPackage() == null)
             return "";
         return "package " + token.getPackageName() + ";\n";
     }
 
+    /**
+     * Returns the implementation of all protected methods in the form of a {@link java.lang.StringBuilder}.
+     *
+     * @param clazz A {@link java.lang.Class} token that requires the implementation of all protected methods.
+     */
     private StringBuilder getProtectedMethods(Class<?> clazz) {
         if (clazz == null)
             return new StringBuilder();
@@ -154,6 +255,11 @@ public class JarImplementor implements JarImpler {
         return result;
     }
 
+    /**
+     * Returns the implementation of all methods in the form of a {@link java.lang.StringBuilder}.
+     *
+     * @param clazz A {@link java.lang.Class} token that requires the implementation of all methods.
+     */
     private StringBuilder getMethods(Class<?> clazz) {
         StringBuilder result = getProtectedMethods(clazz);
         for (Method item : clazz.getMethods())
@@ -228,17 +334,6 @@ public class JarImplementor implements JarImpler {
         } catch (IOException e) {
             System.err.println("An OutPut error occurred:" + e.getMessage());
         }
-    }
-
-    private String toUnicode(String target) {
-        StringBuilder escapeBuilder = new StringBuilder();
-        for (char c : target.toCharArray()) {
-            if (c >= 128)
-                escapeBuilder.append(String.format("\\u%04X", (int) c));
-            else
-                escapeBuilder.append(c);
-        }
-        return escapeBuilder.toString();
     }
 
     /**
---
+++ java-solutions/ru/ifmo/rain/konovalov/implementor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package contains {@link ru.ifmo.rain.konovalov.implementor.JarImplementor} implementation of {@link info.kgeorgiy.java.advanced.implementor.JarImpler} interfaces.
+ *
+ * @author Geny200
+ * @see ru.ifmo.rain.konovalov.implementor.JarImplementor
+ */
+package ru.ifmo.rain.konovalov.implementor;

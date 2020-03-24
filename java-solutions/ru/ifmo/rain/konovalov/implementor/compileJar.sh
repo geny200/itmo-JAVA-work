c9daf405995903fa9fff0d24e9d7b6d1851a6c18
Geny200 <geny200k.other@mail.ru>
2020-03-24 22:33:26 +0300

    Implementor (5 HW) update script

--- java-solutions/ru/ifmo/rain/konovalov/implementor/compileJar.sh
+++ java-solutions/ru/ifmo/rain/konovalov/implementor/compileJar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd ../../../../../
-javac -d temp ./ru/ifmo/rain/konovalov/implementor/JarImplementor.java
-cd temp
+cd ../../../../../ && \
+javac -d temp ./ru/ifmo/rain/konovalov/implementor/JarImplementor.java && \
+cd temp && \
 jar cfm JarImplementor.jar ./../ru/ifmo/rain/konovalov/implementor/Manifest.txt \
      ru/ifmo/rain/konovalov/implementor/*.class \
-     info/kgeorgiy/java/advanced/implementor/*.class
-mv JarImplementor.jar ./../ru/ifmo/rain/konovalov/implementor
-cd ./../
+     info/kgeorgiy/java/advanced/implementor/*.class && \
+mv JarImplementor.jar ./../ru/ifmo/rain/konovalov/implementor && \
+cd ./../ && \
 rm -r -f temp

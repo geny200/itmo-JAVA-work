22f1ec5b21e00a295fa6ed99d803d92b6bf45526
Geny200 <geny200k.other@mail.ru>
2020-03-26 16:50:19 +0300

    Implementor update script fo jar

---
+++ java-solutions/ru/ifmo/rain/konovalov/implementor/compile-javadoc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ../../../../../ && \
+javadoc -d "./ru/ifmo/rain/konovalov/implementor/javadoc" \
+-link https://docs.oracle.com/en/java/javase/13/docs/api \
+-cp "./../../java-advanced-2020/modules/*;./../../java-advanced-2020/lib/*;" \
+-private \
+-author \
+-version ru.ifmo.rain.konovalov.implementor \
+./../../java-advanced-2020/modules/info.kgeorgiy.java.advanced.implementor/info/kgeorgiy/java/advanced/implementor/ImplerException.java \
+./../../java-advanced-2020/modules/info.kgeorgiy.java.advanced.implementor/info/kgeorgiy/java/advanced/implementor/Impler.java \
+./../../java-advanced-2020/modules/info.kgeorgiy.java.advanced.implementor/info/kgeorgiy/java/advanced/implementor/JarImpler.java  \
+>/dev/null

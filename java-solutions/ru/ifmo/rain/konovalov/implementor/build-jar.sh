d927566962179a433d4a03b8d45da0abe1c4e9c5
Geny200 <geny200k.other@mail.ru>
2020-03-26 16:55:41 +0300

    Implementor update script fo jar

--- java-solutions/ru/ifmo/rain/konovalov/implementor/build-jar.sh
+++ java-solutions/ru/ifmo/rain/konovalov/implementor/build-jar.sh
@@ -2,11 +2,11 @@
 mkdir "_build" && \
 javac -d "./_build" -cp "../../../../../../../java-advanced-2020/artifacts/*" "*.java" && \
 cd "./_build" && \
-jar xf ../../../../../../../../java-advanced-2020/artifacts/info.kgeorgiy.java.advanced.implementor.jar \
+jar -xf ../../../../../../../../java-advanced-2020/artifacts/info.kgeorgiy.java.advanced.implementor.jar \
 info/kgeorgiy/java/advanced/implementor/Impler.class \
 info/kgeorgiy/java/advanced/implementor/JarImpler.class \
 info/kgeorgiy/java/advanced/implementor/ImplerException.class && \
-jar cfm _implemetor.jar ./../Manifest.txt \
+jar -cfm _implemetor.jar ./../Manifest.txt \
      ru/ifmo/rain/konovalov/implementor/*.class \
      info/kgeorgiy/java/advanced/implementor/*.class && \
 mv _implemetor.jar ./../ && \

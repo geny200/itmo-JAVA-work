43b6b6f16de58cab6e89b00f375f1b02f6be9fb2
Geny200 <geny200k.other@mail.ru>
2020-03-26 16:40:19 +0300

    Implementor update script fo jar

--- java-solutions/ru/ifmo/rain/konovalov/implementor/build-jar.sh
+++ java-solutions/ru/ifmo/rain/konovalov/implementor/build-jar.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-cd ../../../../../ && \
-javac -d _build ./ru/ifmo/rain/konovalov/implementor/JarImplementor.java && \
-cd _build && \
-jar cfm _implemetor.jar ./../ru/ifmo/rain/konovalov/implementor/Manifest.txt \
+mkdir "_build" && \
+javac -d "./_build" -cp "../../../../../../../java-advanced-2020/artifacts/*" "*.java" && \
+cd "./_build" && \
+jar xf ../../../../../../../../java-advanced-2020/artifacts/info.kgeorgiy.java.advanced.implementor.jar \
+info/kgeorgiy/java/advanced/implementor/Impler.class \
+info/kgeorgiy/java/advanced/implementor/JarImpler.class \
+info/kgeorgiy/java/advanced/implementor/ImplerException.class && \
+jar cfm _implemetor.jar ./../Manifest.txt \
      ru/ifmo/rain/konovalov/implementor/*.class \
      info/kgeorgiy/java/advanced/implementor/*.class && \
-mv JarImplementor.jar ./../ru/ifmo/rain/konovalov/implementor && \
+mv _implemetor.jar ./../ && \
 cd ./../ && \
 rm -r -f _build

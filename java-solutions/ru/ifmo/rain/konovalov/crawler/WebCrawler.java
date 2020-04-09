c725358d6158d2098ab31f308345bd7195d7ef30
Geny200 <geny200k.other@mail.ru>
2020-04-09 12:31:22 +0300

    WebCrawler - completed

--- java-solutions/ru/ifmo/rain/konovalov/crawler/WebCrawler.java
+++ java-solutions/ru/ifmo/rain/konovalov/crawler/WebCrawler.java
@@ -1,22 +1,138 @@
 package ru.ifmo.rain.konovalov.crawler;
 
-import info.kgeorgiy.java.advanced.crawler.CachingDownloader;
-import info.kgeorgiy.java.advanced.crawler.Crawler;
-import info.kgeorgiy.java.advanced.crawler.Downloader;
-import info.kgeorgiy.java.advanced.crawler.Result;
+import info.kgeorgiy.java.advanced.crawler.*;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class WebCrawler implements Crawler {
     Downloader downloader;
     ExecutorService executorDownload;
     ExecutorService executorExtractor;
+
     public WebCrawler(Downloader downloader, int downgrades, int extractors, int ignore) {
         this.executorDownload = Executors.newFixedThreadPool(downgrades);
         this.executorExtractor = Executors.newFixedThreadPool(extractors);
         this.downloader = downloader;
+    }
+
+    protected static class WebCrawlerException extends Exception {
+        WebCrawlerException(String message) {
+            super(message);
+        }
+    }
+
+    private static class Vertex {
+        final String url;
+        final long level;
+
+        Vertex(String url, long level) {
+            this.url = url;
+            this.level = level;
+        }
+
+        String getUrl() {
+            return url;
+        }
+
+        long getLevel() {
+            return level;
+        }
+    }
+
+    private static class DownloadTask {
+        final Downloader downloader;
+        final List<String> downloads;
+        final ConcurrentMap<String, IOException> badDownloads;
+        final ConcurrentMap<String, Boolean> passedUrl ;
+        final BlockingQueue<Vertex> queue;
+        AtomicInteger work;
+        final ExecutorService exeDownload;
+        final ExecutorService exeExtractor;
+
+        public DownloadTask(Downloader downloader, ExecutorService exeDownload, ExecutorService exeExtractor) {
+            this.downloader = downloader;
+            this.exeDownload = exeDownload;
+            this.exeExtractor = exeExtractor;
+            this.work = new AtomicInteger();
+            this.queue = new LinkedBlockingQueue<>();
+            this.badDownloads = new ConcurrentHashMap<>();
+            this.downloads = new LinkedList<>();
+            this.passedUrl = new ConcurrentHashMap<>();
+        }
+
+        private void extract(final Vertex vertex, final Document document) {
+            try {
+                for (String linkUrl : document.extractLinks())
+                    queue.put(new Vertex(linkUrl, vertex.getLevel() + 1));
+                synchronized (downloads) {
+                    downloads.add(vertex.getUrl());
+                }
+            } catch (IOException e) {
+                badDownloads.putIfAbsent(vertex.getUrl(), e);
+            }
+            catch (InterruptedException ignored) {
+
+            }
+            finally {
+                if (work.decrementAndGet() == 0) {
+                    try {
+                        queue.put(vertex);
+                    } catch (InterruptedException ignored) {
+
+                    }
+                }
+            }
+        }
+
+        private void download(final Vertex vertex) {
+            try {
+                final Document document = downloader.download(vertex.url);
+                exeExtractor.submit(() -> extract(vertex, document));
+                work.incrementAndGet();
+            } catch (IOException e) {
+                badDownloads.putIfAbsent(vertex.getUrl(), e);
+            }
+            catch (RejectedExecutionException ignored) {
+
+            }
+            finally {
+                if (work.decrementAndGet() == 0) {
+                    try {
+                        queue.put(vertex);
+                    } catch (InterruptedException ignored) {
+
+                    }
+                }
+            }
+        }
+
+        public void runBFS(String startUrl, int maxDepth) throws InterruptedException {
+            queue.put(new Vertex(startUrl, 1));
+            while (work.get() > 0 || queue.size() > 0) {
+                Vertex localVertex = queue.take();
+                if (localVertex.getLevel() > maxDepth)
+                    continue;
+                if (passedUrl.putIfAbsent(localVertex.getUrl(), true) == null) {
+                    try {
+                        work.incrementAndGet();
+                        exeDownload.submit(() -> {
+                            download(localVertex);
+                        });
+                    }
+                    catch (RejectedExecutionException ignore) {
+
+                    }
+                }
+            }
+        }
+
+        public Result getResult() {
+            return new Result(downloads, badDownloads);
+        }
     }
 
     /**
@@ -28,8 +144,13 @@ public class WebCrawler implements Crawler {
      */
     @Override
     public Result download(String url, int depth) {
-
-        return null;
+        DownloadTask task = new DownloadTask(downloader, executorDownload, executorExtractor);
+        try {
+            task.runBFS(url, depth);
+        } catch (InterruptedException e) {
+            return null;
+        }
+        return task.getResult();
     }
 
     /**
@@ -45,26 +166,44 @@ public class WebCrawler implements Crawler {
             executorExtractor.shutdownNow();
     }
 
-    private static int safeGet(String[] args, int index) {
-        if (args.length <= index)
+    private static int safeGet(String[] args, int index) throws WebCrawlerException {
+        if (args.length <= index) {
             return 1;
-        return  Integer.parseInt(args[index]);
+        }
+        if (args[index] != null) {
+            return Integer.parseInt(args[index]);
+        }
+        throw new WebCrawlerException("Invalid input");
     }
 
     public static void main(String[] args) {
-        if (args == null || args.length < 1 || args.length > 5) {
-            System.out.println("Invalid input");
-            return;
-        }
         try {
-            Downloader downloader = new CachingDownloader();
-            String url = args[0];
-            int depth = safeGet(args, 1), downloads = safeGet(args, 2), extractors = safeGet(args, 3), perHost = safeGet(args, 4);
-            try (WebCrawler webCrawler = new WebCrawler(downloader, downloads, extractors, perHost)) {
-                Result result = webCrawler.download(url, depth);
+            if (args == null || args.length < 1 || args.length > 5) {
+                throw new WebCrawlerException("Invalid input");
             }
-        } catch (IOException e) {
-            System.out.println("Error creating CachingDownloader");
+            try {
+                Downloader downloader = new CachingDownloader();
+                String url = args[0];
+                int depth = safeGet(args, 1), downloads = safeGet(args, 2), extractors = safeGet(args, 3), perHost = safeGet(args, 4);
+                try (WebCrawler webCrawler = new WebCrawler(downloader, downloads, extractors, perHost)) {
+                    Result result = webCrawler.download(url, depth);
+
+                    if (result.getDownloaded().size() > 0) {
+                        System.out.println("\"OK\" urls " + result.getDownloaded().size() + " :");
+                        result.getDownloaded().forEach(System.out::println);
+                    }
+
+                    if (result.getErrors().size() > 0) {
+                        System.out.println("\"Error\" urls " + result.getErrors().size() + " :");
+                        result.getErrors().forEach((s, e) -> System.out.println(s + " - " + e.getMessage()));
+                    }
+                    System.out.println("Finish downloaded");
+                }
+            } catch (IOException e) {
+                throw new WebCrawlerException("Error creating CachingDownloader");
+            }
+        } catch (WebCrawlerException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
